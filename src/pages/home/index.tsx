bc9e62e87e2963209ddd61843d20a39ab2e939a5
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2021-11-30 02:30:31 -0500

    Add spotify now playing/last playing using last.fm

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { DateTime } from 'luxon';
 import styled from 'styled-components';
-import FancyLink from '../../components/FancyLink'
-
+import FancyLink from '../../components/FancyLink';
 
 import { me, me_jpeg } from '../../assets';
-
-const about_me = [
-  <p>I'm a Software Engineer and Data Scientist based in Waterloo, Canada. Over the winter, I worked at <FancyLink className="highlight" href="https://wish.com" target="_blank" rel="noopener noreferrer">Wish</FancyLink> as a Data Scientist on the Consumer Data Team. In the Summmer of 2020, I worked at <FancyLink className="highlight" href="https://wish.com" target="_blank" rel="noopener noreferrer">Wish</FancyLink> as a Software Developer on the Product Payments Team. In my free time, I've helped start a design agency <FancyLink className="highlight" href="https://thesage.co" target="_blank" rel="noopener noreferrer">Sage Co</FancyLink> to help develop websites and brand identities for small businesses.</p>,
-  <p>I have previously worked as a Software Engineer at <FancyLink href="https://kitchenmate.com/" className="highlight" target="_blank" rel="noopener noreferrer">KitchenMate</FancyLink> doing Data Science and Embedded Systems, as a Data Engineer at <FancyLink href="https://draper.ai/" className="highlight" target="_blank"  rel="noopener noreferrer">DraperAI</FancyLink> building smart tools for Amazon Ads and as an Undergraduate Research Assistant under <FancyLink href="https://uwaterloo.ca/electrical-computer-engineering/about/people/karray" className="highlight" target="_blank" rel="noopener noreferrer">Prof. Fakhri Karray</FancyLink> to build intelligents bot for customers at Canada's most popular grocery chain to make it easier to find what they need.</p>,
-  <p>Eight months out of twelve, I study Computer Engineering at the University of Waterloo, and expect to graduate early 2022.</p>,
-  <p>I'm always looking for my next opportunity, so feel free to reach out to me if you want to chat or have any questions!</p>
-]
 
 const socials: {[social: string] : string} = {
   "Email": "mailto:me@arora-aditya.com?Subject=Saying Hello!",
@@ -51,6 +44,51 @@ justify-content: space-between;
 `
 
 export default function Home() {
+  const [nowPlaying, setNowPlaying] = useState<boolean>(false);
+  const [artist, setArtist] = useState<string>("");
+  const [song, setSong] = useState<string>("");
+  const [dataExists, setDataExists] = useState<boolean>(false);
+  const [url, setUrl] = useState<string>("");
+  const [lastListenedAt, setLastListenedAt] = useState<string>("");
+  
+  const about_me = [
+    <p>I'm a Software Engineer and Data Scientist based in Canada. Over the past 2 years I've spent all my coops at <FancyLink className="highlight" href="https://wish.com" target="_blank" rel="noopener noreferrer">Wish</FancyLink> first as a Software Engineer on the Payments team and then as a Data Scientist on the Consumer Data Team. In my free time, I've helped start a design agency <FancyLink className="highlight" href="https://thesage.co" target="_blank" rel="noopener noreferrer">Sage Co</FancyLink> to help develop websites and brand identities for small businesses.</p>,
+    <p>I have previously worked as a Software Engineer at <FancyLink href="https://kitchenmate.com/" className="highlight" target="_blank" rel="noopener noreferrer">KitchenMate</FancyLink> doing Data Science and Embedded Systems, as a Data Engineer at <FancyLink href="https://draper.ai/" className="highlight" target="_blank"  rel="noopener noreferrer">DraperAI</FancyLink> building smart tools for Amazon Ads and as an Undergraduate Research Assistant under <FancyLink href="https://uwaterloo.ca/electrical-computer-engineering/about/people/karray" className="highlight" target="_blank" rel="noopener noreferrer">Prof. Fakhri Karray</FancyLink> to bring NLP chatbots to production and also under <FancyLink href="https://uwaterloo.ca/electrical-computer-engineering/about/people/dw2wrig" className="highlight" target="_blank" rel="noopener noreferrer">Prof. Derek Wright</FancyLink> to build a platform for the ECE department to manage accreditation requirements</p> ,
+    <p>Over the past 4 years, I've studied Computer Engineering at the University of Waterloo, and graduate in early 2022. </p>,
+    <p>Outside of work I like to <FancyLink href="https://blogs.arora-aditya.com/books/" className="highlight" target="_blank">read</FancyLink>, <FancyLink href="https://blogs.arora-aditya.com/" className="highlight" target="_blank">write</FancyLink> and listen to <FancyLink href="https://open.spotify.com/user/arora_aditya?si=771564e93f4a49af" className="highlight" target="_blank">music</FancyLink> ({dataExists ? <span>{nowPlaying ? `currently listening to ` : `last listened to `} <FancyLink href={url} className="highlight">{`${song} by ${artist}`}</FancyLink> {lastListenedAt}</span> : "can't reach spotify yet to get the last song"})</p>,
+    <p>I'm always looking for my next opportunity, so feel free to reach out to me if you want to chat or have any questions!</p>
+  ]
+
+  useEffect(() => {
+    const getData = async () => {
+      var xhr = new XMLHttpRequest();
+      xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
+          let d = JSON.parse(this.responseText);
+          if("@attr" in d && "nowplaying" in d["@attr"]) {
+            setNowPlaying(true);
+          } else {
+            const now = DateTime.now();
+            const ts = DateTime.fromSeconds(Number(d["date"]["uts"]));
+            if(now.diff(ts).as("hours") < 1) {
+              setLastListenedAt(`${Math.round(now.diff(ts).as("minute"))} minutes ago`);
+            } else {
+              setLastListenedAt(`${Math.round(now.diff(ts).as("hour"))} hours ago`);
+            }
+          }
+          setArtist(d["artist"]["#text"]);
+          setSong(d["name"]);
+          setUrl(d["url"]);
+          setDataExists(true);
+        }
+      });
+
+      xhr.open("GET", "https://lichess-proxy.herokuapp.com/spotify");
+      xhr.send();
+    };
+    getData();
+  }, [])
+
   return (
     <HomeContainer>
       <picture>
